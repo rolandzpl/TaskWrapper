855c19a5804fee137f1df229ca00e946df38f4c1
Roland Żerek <rolandz@poczta.fm>
2020-04-18 08:53:29 +0200

    Completed unit tests

--- src/TaskWrapper.Tests/TaskWrapperTests.cs
+++ src/TaskWrapper.Tests/TaskWrapperTests.cs
@@ -1,7 +1,7 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using NUnit.Framework;
-using Unity.Interception.PolicyInjection.Pipeline;
 
 namespace Lithium
 {
@@ -9,21 +9,56 @@ namespace Lithium
     public class TaskWrapperTests
     {
         [Test]
-        public void Test1()
+        public void GetWrapperCreator_TargetTaskCompletes_CompletionCallbackIsCalledAfterTaskCompletes()
         {
             var sw = new Stopwatch();
-            var input = (IMethodInvocation)null;
-            var target = new TaskWrapper();
             sw.Start();
-            var wrappedTask = target.GetWrapperCreator(typeof(int))
+            wrapper
+                .GetWrapperCreator(typeof(int))
                 (
-                    Task.Run(() => { Task.Delay(100).Wait(); return 1; }),
-                    input,
+                    Task.Delay(100).ContinueWith(t => 1),
                     t => sw.Stop(),
                     (t, ex) => sw.Stop()
-                );
-            wrappedTask.Wait();
+                )
+                .Wait();
             Assert.That(sw.ElapsedMilliseconds, Is.GreaterThan(100));
         }
+
+        [Test]
+        public void GetWrapperCreator_TargetTaskCompletes_CanReadResult()
+        {
+            int result = wrapper
+                .GetWrapperCreator(typeof(int))
+                (
+                    Task.FromResult(1),
+                    t => { },
+                    (t, ex) => { }
+                )
+                .GetResult<int>();
+            Assert.That(result, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void GetWrapperCreator_TargetTaskFailsWithException_ErrorCallbackIsCalled()
+        {
+            Exception exception = null;
+            wrapper
+                .GetWrapperCreator(typeof(int))
+                (
+                    Task.FromException(new Exception()),
+                    t => { },
+                    (t, ex) => { exception = ex; }
+                )
+                .Wait();
+            Assert.That(exception, Is.Not.Null);
+        }
+
+        [SetUp]
+        protected void SetUp()
+        {
+            wrapper = new TaskWrapper();
+        }
+
+        private TaskWrapper wrapper;
     }
 }
